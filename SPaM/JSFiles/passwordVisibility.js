e4b811ede191057cf0bb77232b0f22a09c53d293
Adri Massey <adriana.dostine@hotmail.com>
2023-10-20 23:08:30 -0400

    fixed styling and function of the password toggle

--- SPaM/JSFiles/passwordVisibility.js
+++ SPaM/JSFiles/passwordVisibility.js
@@ -1,17 +1,49 @@
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#password");
+// JavaScript for Login Form
+const toggleLoginPassword = document.querySelector("#toggleLoginPassword");
+const loginPassword = document.querySelector("#login-password");
 
-togglePassword.addEventListener("click", function () {
-    // toggle the type attribute
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
+toggleLoginPassword.addEventListener("click", function () {
+    // Toggle the type attribute
+    const type = loginPassword.getAttribute("type") === "password" ? "text" : "password";
+    loginPassword.setAttribute("type", type);
     
-    // toggle the icon
-    this.classList.toggle("bi-eye");
+    // Toggle the icon class
+    if (this.classList.contains("bi-eye-slash")) {
+        this.classList.remove("bi-eye-slash");
+        this.classList.add("bi-eye");
+    } else {
+        this.classList.remove("bi-eye");
+        this.classList.add("bi-eye-slash");
+    }
 });
 
-// prevent form submit
-const form = document.querySelector("form");
-form.addEventListener('submit', function (e) {
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    // You can add your login form handling code here
+});
+
+// JavaScript for Registration Form
+const toggleRegPassword = document.querySelector("#toggleRegPassword");
+const regPassword = document.querySelector("#register-password");
+
+toggleRegPassword.addEventListener("click", function () {
+    // Toggle the type attribute
+    const type = regPassword.getAttribute("type") === "password" ? "text" : "password";
+    regPassword.setAttribute("type", type);
+    
+    // Toggle the icon class
+    if (this.classList.contains("bi-eye-slash")) {
+        this.classList.remove("bi-eye-slash");
+        this.classList.add("bi-eye");
+    } else {
+        this.classList.remove("bi-eye");
+        this.classList.add("bi-eye-slash");
+    }
+});
+
+const regForm = document.querySelector(".register-form");
+regForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // You can add your registration form handling code here
 });
