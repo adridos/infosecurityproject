5ef9c46b4b17c6b54e637755ec26570911d32640
Adri Dostine <66699146+adridos@users.noreply.github.com>
2023-11-28 17:40:12 -0500

    removed some mongodb code

--- SPaM/app.js
+++ SPaM/app.js
@@ -25,40 +25,6 @@ app.use(express.static(__dirname + "/public"));
 app.use("/", homeRouter);
 app.use("/", loginRouter);
 
-// Connect to MongoDB
-const uri = 'ymongodb+srv://adrianadostine:GrilledCheese1996@cluster0.mfau0rr.mongodb.net/'; // Replace with your actual MongoDB connection string
-const dbName = 'spam-db';
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MongoDB:', err);
-    return;
-  }
-  console.log('Connected to MongoDB');
-});
-
-
-app.get('/user/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId;
-
-    const db = client.db(dbName);
-    const collection = db.collection('users');
-
-    // Fetch user by ObjectId
-    const user = await collection.findOne({ _id: new ObjectId(userId) });
-
-    // Render a page with the fetched username
-    res.render('userProfile', { username: user.username });
-  } catch (err) {
-    console.error('Error fetching user:', err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
